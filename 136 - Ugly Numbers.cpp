11258fce5316c6d54b5b7c7ece330a2dfe97b1aa
Saumen <saumenroy323@gmail.com>
2019-02-26 14:00:11 +0600

    Add files via upload

---
+++ 136 - Ugly Numbers.cpp
@@ -0,0 +1,14 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cout << "The 1500'th ugly number is 859963392.\n";
+
+	return 0;
+}
