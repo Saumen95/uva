f10eb84163c06de0e650a76044c7ae0bf0de9dee
Saumen <saumenroy323@gmail.com>
2019-02-26 14:07:34 +0600

    Add files via upload

---
+++ 11526 - H(n).cpp
@@ -0,0 +1,28 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+    
+    int t;
+	long long n;
+    
+    cin >> t;
+	while (t--)
+    {
+        cin >> n;
+    	long long sum = 0;
+    	long long sqN = sqrt(n);
+    	
+    	for (int i = 1; i <= sqN; ++i)
+    		sum += n / i;
+    
+    	cout << (sum << 1) - sqN * sqN << "\n";
+    }
+    
+	return 0;
+}
